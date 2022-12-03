b83df2c7cf7267d2c21e26575523a59b032afffc
pedrosoares <pedrinhocef@gmail.com>
2022-12-03 15:10:14 -0300

    refatorando alguns testes para usar o mockk, e adicionando outros cenários de testes possíveis na viewModel

--- homecards/src/test/java/com/challange/homecards/view/viewmodel/HeartStoneViewModelTest.kt
+++ homecards/src/test/java/com/challange/homecards/view/viewmodel/HeartStoneViewModelTest.kt
@@ -4,13 +4,17 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.challange.homecards.data.usecase.HeartStoneUseCaseImpl
 import com.challange.homecards.helper.Resource
 import com.challange.network.model.HeartStoneRareResponseItem
-import com.challange.network.model.MechanicsItem
 import com.nhaarman.mockitokotlin2.doReturn
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import okhttp3.ResponseBody
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.After
 import org.junit.Before
@@ -27,14 +31,16 @@ internal class HeartStoneViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private var useCaseMockK = mockk<HeartStoneUseCaseImpl>()
     private var useCaseMock =  Mockito.mock(HeartStoneUseCaseImpl::class.java)
     private lateinit var viewModel : HeartStoneViewModel
+    private var listRareItems = mockk<List<HeartStoneRareResponseItem>>()
 
 
     @Before
     fun setup() {
         Dispatchers.setMain(Dispatchers.Unconfined)
-        viewModel = HeartStoneViewModel(useCaseMock)
+        viewModel = HeartStoneViewModel(useCaseMockK)
     }
 
     @After
@@ -45,11 +51,20 @@ internal class HeartStoneViewModelTest {
     @Test
     fun `should return success when call get heart stone live data` () = runTest {
 
-        doReturn(mockResponseSuccess()).`when`(useCaseMock).getHeartStoneResponseUseCase()
+        coEvery { useCaseMockK.getHeartStoneResponseUseCase() } returns mockResponseSuccess()
 
         viewModel.getHeartStoneResponse()
 
-        viewModel.heartStone.value.shouldBeEqualTo(Resource.success(listRareItems()))
+        viewModel.heartStone.value.shouldBeEqualTo(Resource.success(listRareItems))
+    }
+
+    @Test
+    fun `should filtering a valid list when called` () = runTest {
+        every { useCaseMockK.filterList(listRareItems) } returns listRareItems
+
+        viewModel.filteringListOnlyValidImages(listRareItems)
+
+        verify (exactly = 1) { useCaseMockK.filterList(listRareItems) }
     }
 
     @Test
@@ -62,6 +77,38 @@ internal class HeartStoneViewModelTest {
         viewModel.errorDataNull.value.shouldBeEqualTo(null)
     }
 
+
+    @Test
+    fun `should return a list from cache when call getCache`() = runTest {
+
+        coEvery { useCaseMockK.getCache() } returns listRareItems
+
+        viewModel.getHeartStoneCacheCache()
+
+        verify (exactly = 1) { viewModel.getHeartStoneCacheCache() }
+    }
+
+    @Test
+    fun `should add a list to cache when call add cache`() = runTest {
+
+        coEvery { useCaseMockK.addCache(listRareItems) } returns (Unit)
+
+        viewModel.addCache(listRareItems)
+
+        verify (exactly = 1) { viewModel.addCache(listRareItems) }
+    }
+
+
+    @Test
+    fun `should delete a list from cache when call delete cache`() = runTest {
+
+        coEvery { useCaseMockK.deleteCache() } returns (Unit)
+
+        viewModel.deleteCache()
+
+        verify (exactly = 1) { viewModel.deleteCache() }
+    }
+
     @Test(expected = MockitoException::class)
     fun `should return IOException when call get heart`() = runTest {
 
@@ -72,17 +119,7 @@ internal class HeartStoneViewModelTest {
         viewModel.heartStone.value.shouldBeEqualTo(Resource.error<HeartStoneRareResponseItem>(IOException()))
     }
 
-    private fun listRareItems(): List<HeartStoneRareResponseItem> =
-        arrayListOf(HeartStoneRareResponseItem(
-            img = "", cost = 0, collectible = false, artist = "", faction = "",
-            flavor = "", health = 0, mechanics = arrayListOf(MechanicsItem(name = "")),
-            dbfId = 0, type = "", locale = "", playerClass = "", cardId = "", cardSet = "",
-            attack = 0, name = "", imgGold = "", text = "", rarity = "", spellSchool = "",
-            race = "", howToGet = "", howToGetGold = "", durability = 0, classes = arrayListOf(toString()),
-            multiClassGroup = "", elite = false, armor = ""
-        ))
+    private fun mockResponseSuccess() = Response.success(listRareItems)
 
-    private fun mockResponseSuccess() = Response.success(listRareItems())
-
-    private fun mockResponseNull() = null
+    private fun mockResponseNull(): ResponseBody? = null
 }
